f502308c2f2daa19991b46ca06f5d495df67322f
Fabian Schöttler <fabian@schoettlerhome.de>
2016-10-14 14:45:10 +0200

    halloweenstick: fix creation of zeroamount stack

--- src/main/java/net/scottec/TrickOrTreat/Items/Halloweenstick/Halloweenstick.java
+++ src/main/java/net/scottec/TrickOrTreat/Items/Halloweenstick/Halloweenstick.java
@@ -9,6 +9,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class Halloweenstick extends ItemStack {
     public Halloweenstick() {
         this.setType(Material.BLAZE_ROD);
+        this.setAmount(1);
         ItemMeta im = this.getItemMeta();
         im.setDisplayName(util.getString("HALLOWEENSTICK_NAME"));
         im.setLore(util.getStringList("HALLOWEENSTICK_LORE"));
