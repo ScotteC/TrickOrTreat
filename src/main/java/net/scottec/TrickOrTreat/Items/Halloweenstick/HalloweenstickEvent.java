dea8f834b33f697866498ccce5f94ff6edb750af
Fabian Schöttler <fabian@schoettlerhome.de>
2016-10-14 14:45:50 +0200

    halloweenstickevent: remove eventmamber 'hand', not needed

--- src/main/java/net/scottec/TrickOrTreat/Items/Halloweenstick/HalloweenstickEvent.java
+++ src/main/java/net/scottec/TrickOrTreat/Items/Halloweenstick/HalloweenstickEvent.java
@@ -3,7 +3,6 @@ package net.scottec.TrickOrTreat.Items.Halloweenstick;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
-import org.bukkit.inventory.EquipmentSlot;
 
 
 public class HalloweenstickEvent extends Event {
@@ -11,12 +10,10 @@ public class HalloweenstickEvent extends Event {
 
     private Player player;
     private Player target;
-    private EquipmentSlot hand;
 
-    public HalloweenstickEvent(Player player, Player target, EquipmentSlot hand) {
+    public HalloweenstickEvent(Player player, Player target) {
         this.player = player;
         this.target = target;
-        this.hand = hand;
     }
 
     public Player getPlayer() {
@@ -25,10 +22,6 @@ public class HalloweenstickEvent extends Event {
 
     public Player getTarget() {
         return this.target;
-    }
-
-    public EquipmentSlot getHand() {
-        return this.hand;
     }
 
     public HandlerList getHandlers() {
