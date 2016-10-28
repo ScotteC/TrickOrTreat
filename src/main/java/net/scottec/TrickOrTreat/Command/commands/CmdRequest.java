85e042c362733afa3fd1c55536d5afb4e4ef645e
Fabian Schöttler <fabian@schoettlerhome.de>
2016-10-28 04:47:20 +0200

    CmdRequest: pack cooldown and timeout into one single command

--- src/main/java/net/scottec/TrickOrTreat/Command/commands/CmdRequest.java
+++ src/main/java/net/scottec/TrickOrTreat/Command/commands/CmdRequest.java
@@ -3,16 +3,12 @@ package net.scottec.TrickOrTreat.Command.commands;
 import net.scottec.TrickOrTreat.Command.CommandTop;
 import net.scottec.TrickOrTreat.TrickOrTreat;
 
-/**
- * Created by Fabian on 23.10.2016.
- */
 public class CmdRequest extends CommandTop {
 
     public CmdRequest(TrickOrTreat.ITrickOrTreat iToT) {
         super(iToT, "request");
         this.setCommandUsage("/tot request <args> : Handling requests");
 
-        this.addSubCommand(new CmdRequestSetCooldown(iToT));
-        this.addSubCommand(new CmdRequestSetTimeout(iToT));
+        this.addSubCommand(new CmdRequestSetTiming(iToT));
     }
 }
---
+++ src/main/java/net/scottec/TrickOrTreat/Command/commands/CmdRequestSetTiming.java
@@ -0,0 +1,34 @@
+package net.scottec.TrickOrTreat.Command.commands;
+
+import net.scottec.TrickOrTreat.Command.CommandBase;
+import net.scottec.TrickOrTreat.TrickOrTreat;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class CmdRequestSetTiming extends CommandBase{
+
+    public CmdRequestSetTiming(TrickOrTreat.ITrickOrTreat iToT) {
+        super(iToT, "timing");
+        this.setCommandUsage("/tot request timing <timeout> <cooldown> : Set timings for requests");
+    }
+
+    @Override
+    public void execute(Player sender, List<String> args) {
+        if(sender.hasPermission("trickortreat.config")) {
+            if(args != null && args.size() >= 2){
+                try {
+                    this.plugin.getRequestHandler().setRequestTimings(
+                            Integer.parseInt(args.get(0)),
+                            Integer.parseInt(args.get(1)));
+                } catch (NumberFormatException e) {
+                    sender.sendMessage("Invalid input");
+                }
+            }
+            else
+                sender.sendMessage("Not enough arguments (2 needed)");
+        }
+        else
+            sender.sendMessage("No Permission");
+    }
+}
