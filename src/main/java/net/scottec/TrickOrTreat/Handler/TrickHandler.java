f981a144a5b5e5a4ad0a9f0fe89248a7614a2dc5
Fabian Schöttler <fabian@schoettlerhome.de>
2016-10-31 00:04:59 +0100

    TrickHandler: get access to tricks in hashmap via names, choose randomTrick via paralell list

--- src/main/java/net/scottec/TrickOrTreat/Handler/TrickHandler.java
+++ src/main/java/net/scottec/TrickOrTreat/Handler/TrickHandler.java
@@ -5,22 +5,33 @@ import net.scottec.TrickOrTreat.Tricks.*;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TrickHandler {
-    private static final List<Trick> tricks = new ArrayList<>();
+    private static final List<String> trickNames = new ArrayList<>();
+    private static final Map<String, Trick> tricks = new HashMap<>();
 
     public TrickHandler(TrickOrTreat.ITrickOrTreat iToT) {
-        tricks.add(new Explosion());
-        tricks.add(new Teleport());
-        tricks.add(new Flames());
-        tricks.add(new Lightning());
-        tricks.add(new Pumpkin(iToT));
+        addTrick(new Explosion(), "explosion");
+        addTrick(new Teleport(), "teleport");
+        addTrick(new Flames(), "flames");
+        addTrick(new Lightning(), "lightning");
+        addTrick(new Pumpkin(iToT), "pumpkin");
+    }
+
+    private void addTrick(Trick trick, String trickName) {
+        tricks.put(trickName, trick);
+        trickNames.add(trickName);
+    }
+
+    public Trick getTrickByName(String name) {
+        return tricks.get(name);
     }
 
     public void trick(Player player) {
         if (player != null)
-            tricks.get(((int) (Math.random() * 100)) % tricks.size())
-                    .effect(player);
+            tricks.get(trickNames.get((int)(Math.random() * 100) % trickNames.size())).effect(player);
     }
 }
